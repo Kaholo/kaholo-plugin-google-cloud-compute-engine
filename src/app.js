a322dbeafcdfdd7c849dd75cef0004b1c91c319c
Ken Marsh <kenskyfish@gmail.com>
2022-01-20 16:21:33 +0700

    machineType.id is a number. machineType.value is a string like e2-micro.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ async function launchVm(action, settings) {
     
     const customCpu = parsers.number(action.params.customMachineCpuCount);
     const customMem = parsers.number(action.params.customMachineMem);
-    let machineType = parsers.autocomplete(action.params.machineType) || "custom-";
+    let machineType = parsers.autocomplete(action.params.machineType.value) || "custom-";
     if (machineType.includes('custom')){
         if (!customCpu || !customMem) {
             throw "Must provide both CPU Count and memory size for custom machine type.";
